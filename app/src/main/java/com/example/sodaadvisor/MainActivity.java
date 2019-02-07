40f863236e39a939013707addb3ace06bf55c992
0ne <S00033292@auk.edu.kw>
2019-02-07 17:31:58 +0300

    modified:   app/src/main/java/com/example/sodaadvisor/MainActivity.java 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/sodaadvisor/MainActivity.java
+++ app/src/main/java/com/example/sodaadvisor/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.sodaadvisor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner sp;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickBtn(View v) {
+        //code for the onclick
+        Integer.valueOf()
+        sp = (Spinner)findViewById(R.id.spinner);
+        tv = (TextView)findViewById(R.id.textView);
+
+        sp.getSelectedItem()
     }
 }
