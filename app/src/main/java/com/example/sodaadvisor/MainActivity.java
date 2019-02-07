9ce7525e11c7e5cb1de244ac8e392fc22046b9b7
0ne <S00033292@auk.edu.kw>
2019-02-07 17:49:02 +0300

    modified:   app/src/main/java/com/example/sodaadvisor/MainActivity.java

--- app/src/main/java/com/example/sodaadvisor/MainActivity.java
+++ app/src/main/java/com/example/sodaadvisor/MainActivity.java
@@ -7,9 +7,12 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Spinner sp;
     TextView tv;
+    sodaExpert expert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +22,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickBtn(View v) {
         //code for the onclick
+        ArrayList<String> List = new ArrayList<String>();
         sp = (Spinner)findViewById(R.id.spinner);
         tv = (TextView)findViewById(R.id.textView);
         String soda_type = String.valueOf(sp.getSelectedItem());
-
+        expert = new sodaExpert();
+        List = expert.getFlavour(soda_type);
     }
 }
